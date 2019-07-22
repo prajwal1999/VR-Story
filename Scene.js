6e475d6af9bd3f1046c3f265f65e06b62365ad7f
prajwal1999 <prajwaldkamble1998@gmail.com>
2019-07-22 13:55:43 +0530

    added navigation in scene 2

--- Scene.js
+++ Scene.js
@@ -13,7 +13,7 @@ var Scene2Info = [];
         var xLabel="X: "+ currX.toFixed(2);
         var yLabel="Y: "+ currY.toFixed(2);
         var skyElement = document.querySelector('#background');
-        // console.log(xLabel, yLabel)
+        console.log(xLabel, yLabel)
         if(Scene === "Scene1") {
 
           if( (currX > 346 || currX < 11) && (currY > 65 && currY < 100) && Scene === "Scene1" ) {
@@ -89,8 +89,51 @@ var Scene2Info = [];
               document.querySelector("#spacial_box2_6").click()
             }
           }
+        } else if(Scene === "Scene3") {
+          if( (currX > 313 || currX < 20) && (currY > 229 && currY < 261) ) {
+            skyElement.setAttribute('src', '#scene31')
+            // document.querySelector('#audio_image3_1').setAttribute('position', "47 28 80")
+          } else if( (currX > 318 || currX < 16) && (currY > 143 && currY < 173) ) {
+            skyElement.setAttribute('src', '#scene32')
+          //   document.querySelector('#audio_image2_2').setAttribute('position', "55 25 -75")
+          } else if( (currX > 341 || currX < 18) && (currY > 76 && currY < 120) ) {
+            skyElement.setAttribute('src', '#scene33')
+          //   document.querySelector('#audio_image2_3').setAttribute('position', "4 -20 92")
+          }  //else if( (currX > 30 && currX < 40) && (currY > 127 && currY < 133) ) {
+          //   skyElement.setAttribute('src', '#scene24')
+          //   document.querySelector('#audio_image2_4').setAttribute('position', "-63 64 39")
+          // }  else if( (currX > 354 || currX < 25) && (currY > 45 && currY < 70) ) {
+          //   skyElement.setAttribute('src', '#scene25')
+          //   document.querySelector('#audio_image2_5').setAttribute('position', "-92 27 -20")
+          // } else if( (currX > 22 && currX < 41) && (currY > 357 || currY < 10) ) {
+          //   skyElement.setAttribute('src', '#scene26')
+          //   document.querySelector('#audio_image2_6').setAttribute('position', "8 60 -76")
+          // }
+          else {
+            skyElement.setAttribute('src', '#scene3BW')
+            // document.querySelector('#audio_image2_1').setAttribute('position', "47 50 80")
+            // document.querySelector('#audio_image2_2').setAttribute('position', "55 25 -92")
+            // document.querySelector('#audio_image2_3').setAttribute('position', "4 -20 100")
+            // document.querySelector('#audio_image2_4').setAttribute('position', "-63 73 39")
+            // document.querySelector('#audio_image2_5').setAttribute('position', "-100 27 -20")
+            // document.querySelector('#audio_image2_6').setAttribute('position', "8 60 -84")
+            let randNum = Math.random()
+              
+            // if ( (randNum > 0.99) && (Scene2Info.indexOf("audio2_1") === -1) &&  Scene === "Scene2") { console.log(randNum)
+            //     document.querySelector("#spacial_box2_1").click()
+            // } else if ( (randNum < 0.01) && (Scene2Info.indexOf("audio2_2") === -1) &&  Scene === "Scene2") { console.log(randNum)
+            //     document.querySelector("#spacial_box2_2").click()
+            // } else if ( (randNum > 0.10) && (randNum < 0.11) && (Scene2Info.indexOf("audio2_3") === -1) &&  Scene === "Scene2") { console.log(randNum)
+            //   document.querySelector("#spacial_box2_3").click()
+            // } else if ( (randNum > 0.20) && (randNum < 0.21) && (Scene2Info.indexOf("audio2_4") === -1) &&  Scene === "Scene2") { console.log(randNum)
+            //   document.querySelector("#spacial_box2_4").click()
+            // } else if ( (randNum > 0.30) && (randNum < 0.31) && (Scene2Info.indexOf("audio2_5") === -1) &&  Scene === "Scene2") { console.log(randNum)
+            //   document.querySelector("#spacial_box2_5").click()
+            // } else if ( (randNum > 0.40) && (randNum < 0.41) && (Scene2Info.indexOf("audio2_6") === -1) &&  Scene === "Scene2") { console.log(randNum)
+            //   document.querySelector("#spacial_box2_6").click()
+            // }
+          }
         }
-
 
         if(Scene1Info.length === 4 && Scene === "Scene1") {
           document.querySelector('#arrow_image1_1').setAttribute('position', "-89 33 -26")
@@ -98,6 +141,14 @@ var Scene2Info = [];
         } else {
           document.querySelector('#arrow_image1_1').setAttribute('position', "-94 30 -26")
           document.querySelector('#arrow_image1_4').setAttribute('position', "-37 14 94")
+        }
+
+        if(Scene2Info.length === 6 && Scene === "Scene2") {
+          document.querySelector('#arrow_image2_1').setAttribute('position', "47 22 80")
+          document.querySelector('#arrow_image2_2').setAttribute('position', "55 25 -68")
+        } else {
+          document.querySelector('#arrow_image2_1').setAttribute('position', "47 50 80")
+          document.querySelector('#arrow_image2_2').setAttribute('position', "55 25 -92")
         }
 
 
--- SceneNavigation.js
+++ SceneNavigation.js
@@ -16,4 +16,49 @@ document.querySelector("#arrow_image1_1").addEventListener('click', async()=>{
     document.querySelector('#arrow_image1_4').setAttribute('position', "-37 14 94")
     skyElement.setAttribute('src', '#scene2BW')
     Scene = "Scene2"
-  })
+})
+
+document.querySelector("#arrow_image2_1").addEventListener('click', async()=>{
+  const skyElement = document.querySelector('#background');
+  let i = 1;
+        while(i<2){
+          await sleep(25).then(()=>{
+            i=i+0.01;
+            document.querySelector('#camera').setAttribute('zoom', i)
+          })
+        }
+  document.querySelector('#audio_image2_1').setAttribute('position', "47 50 80")
+  document.querySelector('#arrow_image2_1').setAttribute('position', "47 50 80")
+  document.querySelector('#audio_image2_2').setAttribute('position', "55 25 -92")
+  document.querySelector('#arrow_image2_2').setAttribute('position', "55 25 -92")
+  document.querySelector('#audio_image2_3').setAttribute('position', "4 -20 100")
+  document.querySelector('#audio_image2_4').setAttribute('position', "-63 73 39")
+  document.querySelector('#audio_image2_5').setAttribute('position', "-100 27 -20")
+  document.querySelector('#audio_image2_5').setAttribute('position', "8 60 -84")
+  
+  skyElement.setAttribute('src', '#scene3BW')
+  Scene = "Scene3"
+})
+
+
+document.querySelector("#arrow_image2_2").addEventListener('click', async()=>{
+  const skyElement = document.querySelector('#background');
+  let i = 1;
+        while(i<2){
+          await sleep(25).then(()=>{
+            i=i+0.01;
+            document.querySelector('#camera').setAttribute('zoom', i)
+          })
+        }
+  document.querySelector('#audio_image2_1').setAttribute('position', "47 50 80")
+  document.querySelector('#arrow_image2_1').setAttribute('position', "47 50 80")
+  document.querySelector('#audio_image2_2').setAttribute('position', "55 25 -92")
+  document.querySelector('#arrow_image2_2').setAttribute('position', "55 25 -92")
+  document.querySelector('#audio_image2_3').setAttribute('position', "4 -20 100")
+  document.querySelector('#audio_image2_4').setAttribute('position', "-63 73 39")
+  document.querySelector('#audio_image2_5').setAttribute('position', "-100 27 -20")
+  document.querySelector('#audio_image2_5').setAttribute('position', "8 60 -84")
+  
+  skyElement.setAttribute('src', '#scene1BW')
+  Scene = "Scene1"
+})
